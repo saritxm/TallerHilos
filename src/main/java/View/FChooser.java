bd283475935718919a740e0cdd01ebbce3ae67cb
saritxm <saranicollemm@gmail.com>
2024-04-13 16:25:36 -0500

    mm

--- src/main/java/View/FChooser.java
+++ src/main/java/View/FChooser.java
@@ -1,11 +1,34 @@
 package View;
 
+import java.io.File;
+
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class FChooser {
-    public JFileChooser fDirec; 
+public class FChooser { 
 
-public FChooser() {
-    // Constructor vacío
-}
+    public FChooser() {
+        // Constructor vacío
+    }
+
+    public String seleccionarDirectorio() {
+        String[] opcionesDir = {"Cargar Directorio"};
+        int opc2 = JOptionPane.showOptionDialog(null, "Seleccione una carpeta con imagenes:", "Subir Directorio", 0, JOptionPane.QUESTION_MESSAGE, null, opcionesDir, "");
+        if (opc2 == 0) {
+            JFileChooser jf = new JFileChooser();
+            jf.setMultiSelectionEnabled(false);
+            FileNameExtensionFilter filtro = new FileNameExtensionFilter("Archivos de imagen", "jpg", "png");
+            jf.setFileFilter(filtro);
+
+            int resultado = jf.showOpenDialog(null);
+
+            if (resultado == JFileChooser.APPROVE_OPTION) {
+                return jf.getSelectedFile().getAbsolutePath();
+            } else {
+                JOptionPane.showMessageDialog(null, "No se ha seleccionado una carpeta.", "Aviso", JOptionPane.WARNING_MESSAGE);
+            }
+        }
+        return null;
+    }
 }
